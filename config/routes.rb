d76ce8aa09f2e93227f2ccac6de9aac47841490c
Maud de La Gournerie mdelagournerie@live.fr <mdelagournerie@live.fr>
2020-03-31 16:37:51 +0200

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
 
   get "/tasks/:id", to: "tasks#show", as: :task
 
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
 end
